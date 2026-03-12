3fd1ca1dee3364f476c0f73a4583728cc6dd4b4d
lelocle3924 <166092601+lelocle3924@users.noreply.github.com>
2026-03-12 12:21:42 +0700

    fix: move GitHub Actions workflows to correct directory

--- push.bat
+++ push.bat
@@ -1,2 +1,8 @@
-git commit -m "feat: migrate to Taiwanese Traditional Chinese and add browser TTS"
-git push origin main
+git status
+git commit -am "fix: move GitHub Actions workflows to correct directory"
+git push origin master
+
+git push --delete origin v1.1.0
+git tag -d v1.1.0
+git tag v1.1.0
+git push origin v1.1.0
